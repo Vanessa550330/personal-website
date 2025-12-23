e0e2cfe5d558437e3bcbdd433597df2c9145d64e
Vanessa Lo <valerie550330@yahoo.com.tw>
2025-12-23 22:59:59 +0800

    Resolve merge conflict in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -8,15 +7,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> ceae85f2bd2cb576666b85d7717e3d73fd05d4e1
 )
